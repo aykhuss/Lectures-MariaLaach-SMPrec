1d5c893e25b6d67b504617089172bd7ca554a412
Alexander Huss <alexander.huss@cern.ch>
2023-08-25 11:18:58 +0200

    Update toy shower documentation and implementation. use transverse-momentum ordering, include more information on the derivation, add more comparisons in the plots.

--- org/toy-shower/main.py
+++ org/toy-shower/main.py
@@ -3,44 +3,52 @@
 import math
 import random
 import sys
+import numpy as np
 
 random.seed(42)
 alphas = 0.118
 
-def generate_event(Q2_start: float, Q2_cutoff: float, CX: float):
-  sudakov = 1.  # initialize Sudakov to the starting scale
-  fac = alphas*CX/(2.*math.pi)
-  Qlist = []
-  while True:
-    r = random.uniform(0.,1.)
-    sudakov *= r
-    #> sudakov = exp( -[alphas*CX/(2.*pi)] * log^2[Q2/Q2_start] )
-    #> determine Q2 from the associated sudakov
-    L2 = - math.log(sudakov) / fac
-    Q2 = Q2_start * math.exp(-math.sqrt(L2))
-    if Q2 < Q2_cutoff:
-      break
-    Qlist.append( math.sqrt(Q2) )
-  if len(Qlist) > 1:
-    print("#summary2 {} {} {} {}".format(len(Qlist),sum(Qlist),Qlist[0],Qlist))
+
+def generate_emissions(kt_max: float, kt_cut: float, CX: float) -> list[float]:
+    emissions = list()
+    fac = CX * alphas / math.pi  # save common factor
+    sudakov = 1.  # initialize to the starting scale
+    while True:
+        sudakov *= random.uniform(0., 1.)
+        #> invert `r = sudakov(kt, kt_new)`
+        L2 = -math.log(sudakov) / fac
+        kt = kt_max * math.exp(-math.sqrt(L2))
+        if kt <= kt_cut:
+            break
+        emissions.append(kt)
+    return emissions
+
 
 if __name__ == "__main__":
-  if len(sys.argv) < 3:
-    raise RuntimeError("I expect at least two arguments:  Q_start [g|q]")
-  Q_start = float(sys.argv[1])  # the hard scale
-  Q_cutoff = 1  # shower cutoff (PS stops -> hand over to hadronization)
-  if sys.argv[2] == "q":
-    CX = 4./3.  # quark
-  elif sys.argv[2] == "g":
-    CX = 3.     # gluon
-  else:
-    raise RuntimeError("unrecognised parton: {}".format(sys.argv[2]))
-  if len(sys.argv) >= 4:
-    alphas = float(sys.argv[3])
-  if len(sys.argv) >= 5:
-    nevents = int(sys.argv[4])
-  else:
-    nevents = 1000
-  for i in range(nevents):
-    print("# event {} [{} {} {} {} {}]".format(i,Q_start,sys.argv[2],CX,alphas,nevents))
-    generate_event(Q_start**2, Q_cutoff**2, CX)
+    if len(sys.argv) < 3:
+        raise RuntimeError("I expect at least two arguments:  kt_max [g|q]")
+    kt_max = float(sys.argv[1])  # the hard scale
+    kt_cut = 1.  # shower cutoff
+    if sys.argv[2] == "q":
+        CX = 4. / 3.
+    elif sys.argv[2] == "g":
+        CX = 3.
+    else:
+        raise RuntimeError("unrecognised parton: {}".format(sys.argv[2]))
+    if len(sys.argv) >= 4:
+        alphas = float(sys.argv[3])
+    if len(sys.argv) >= 5:
+        nevents = int(sys.argv[4])
+    else:
+        nevents = 1000
+    #> define a log histogram
+    xs = [x for x in np.logspace(-5, 0, 50)]
+    for i in range(nevents):
+        print("#event {} [{} {} {} {} {}]".format(i, kt_max, sys.argv[2], CX,
+                                                  alphas, nevents))
+        emissions = generate_emissions(kt_max, kt_cut, CX)
+        if len(emissions) > 0:
+            print("#summary {} {} {} {} {}".format(
+                len(emissions), sum(emissions),
+                math.log(sum(emissions) / kt_max), emissions[0],
+                math.log(emissions[0] / kt_max)))
